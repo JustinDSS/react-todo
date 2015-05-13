b8228c3bc1cd585d0d30cb33814d28d6c2553905
Kevin Welcher <kaw2k@virginia.edu>
2015-05-13 14:16:45 -0700

    Adding graphs

--- client/src/js/handlers/app.js
+++ client/src/js/handlers/app.js
@@ -5,13 +5,58 @@ var { RouteHandler } = require('react-router');
 var StoreWatchComponent = require('components/storeWatch');
 var { flux } = require('flux');
 var { Link } = require('react-router');
+var _ = require('ramda');
+
+var DoughnutChart = require("react-chartjs").Doughnut;
 
 var App = React.createClass({
+
+    renderChart: function() {
+
+        var data = _.compose(
+            _.mapObj(_.length),
+            _.groupBy(task => task.complete ? 'Complete' : 'Pending')
+        )(this.props.tasks);
+
+        // If we are empty, simulate having one pending task.
+        // This way we still have a graph on the screen from the getgo
+        if (!data.Pending && !data.Complete) {
+            data.Pending = 1;
+        }
+
+        // Setup the chart data with what gets actually displayed to the screen.
+        // If we don't have any pending or complete, default them to
+        // 0 instead of undefined
+        var chartData = [
+            {
+                value: data.Pending || 0,
+                color:"#D9F5FE",
+                highlight: "#D9F5FE",
+                label: "Pending"
+            },
+            {
+                value: data.Complete || 0,
+                color:"#009ECD",
+                highlight: "#009ECD",
+                label: "Completed"
+            }
+        ];
+
+        var options = {
+            animationEasing : "easeInOutQuart",
+            segmentStrokeWidth : 1,
+            showTooltips: false
+        };
+
+        return <DoughnutChart className="completed-graph" data={chartData} options={options}/>;
+    },
 
     render: function() {
         return <div className="app-container">
             <div className="header">
-                <Link className="title" to="tasks">TODO</Link>
+                <Link className="title" to="tasks">
+                    TOD{this.renderChart()}
+                </Link>
                 <Link className="add" to="add">
                     add
                 </Link>
--- client/src/js/handlers/taskList.js
+++ client/src/js/handlers/taskList.js
@@ -6,13 +6,61 @@ var TaskItem = require('handlers/taskItem');
 
 var TaskList = React.createClass({
 
+    getInitialState: function() {
+        return {
+            sort: 'az'
+        };
+    },
+
     componentWillMount: function() {
         flux.actions.all();
     },
 
+    onChange: function() {
+        var checked = this.refs.az.getDOMNode().checked ? 'az' : 'completed';
+
+        this.setState({
+            sort: checked
+        });
+    },
+
     render: function() {
+        var sort = this.state.sort;
+
+        var tasks = this.props.tasks.sort(function(a, b) {
+            if (sort === 'az') {
+                return a.text.localeCompare(b.text);
+            }
+
+            if (sort === 'completed') {
+                return a.complete.toString().localeCompare(b.complete.toString());
+            }
+        });
+
         return <div className="list-container">
-            {this.props.tasks.map((task) => {
+            <form className="radio" onChange={this.onChange}>
+                Sort by:
+
+                <label>
+                    <input
+                        type="radio"
+                        ref="az"
+                        name="sort"
+                        value="az"
+                        defaultChecked />
+                    <span>Alphabetical</span>
+                </label>
+
+                <label>
+                    <input
+                        type="radio"
+                        name="sort"
+                        value="completed" />
+                    <span>Completed</span>
+                </label>
+            </form>
+
+            {tasks.map((task) => {
                 return <TaskItem task={task} />;
             })}
         </div>;
